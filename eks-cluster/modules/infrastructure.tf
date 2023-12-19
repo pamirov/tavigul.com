40aa6f3c64e752214439247a626456760f95763a
Abai <pamirovkr@gmail.com>
2023-12-18 21:56:52 -0600

    let's do it again

--- eks-cluster/modules/infrastructure.tf
+++ eks-cluster/modules/infrastructure.tf
@@ -1,7 +1,5 @@
 resource "aws_vpc" "cluster_vpc" {
     cidr_block = var.cidr_vpc
-
-    tags = var.vpc_tags
 }
 
 
--- eks-cluster/modules/variables.tf
+++ eks-cluster/modules/variables.tf
@@ -4,8 +4,4 @@ variable "cidr_vpc" {
     default = "10.0.0.0/16"
 }
 
-variable "vpc_tags" {
-    default = {
-        Name = "cluster_vpc"
-    }
-}
+
