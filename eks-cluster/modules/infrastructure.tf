08bd38256d515bb1d559c92af78988128fdc6112
Abai <pamirovkr@gmail.com>
2023-12-18 20:52:17 -0600

    added sample vpc child module

---
+++ eks-cluster/modules/infrastructure.tf
@@ -0,0 +1,4 @@
+resource "aws_vpc" "cluster_vpc" {
+    cidr_block = var.cidr_vpc
+}
+
---
+++ eks-cluster/modules/variables.tf
@@ -0,0 +1,7 @@
+#Child module variables with default values
+
+variable "cidr_vpc" {
+    default = "10.0.0.0/16"
+}
+
+
